d711aa98feeb4572b429996924f81e2b8db532dd
Kyle Kingsbury <aphyr@aphyr.com>
2026-07-30 19:53:14 -0500

    Jepsen 0.3.13

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.5" :scope "provided"]
-                 [jepsen "0.3.12-SNAPSHOT"]
+                 [jepsen "0.3.13"]
                  [com.github.seancorfield/next.jdbc "1.3.1093"]
                  [com.gfredericks/test.chuck "0.2.15"]
                  [org.clojure/test.check "1.1.3"]]
